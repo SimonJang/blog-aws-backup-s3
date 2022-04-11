d6e9c5daf1b4bde389436efe6fb3f7e573492fd0
Simon Jang <simon.jang.contact@gmail.com>
2022-04-11 19:57:40 +0200

    feat: Add AWS Backup role

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,7 +2,7 @@ import {Stack, StackProps, RemovalPolicy, Duration, Tags} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {Bucket, BlockPublicAccess, ObjectOwnership} from 'aws-cdk-lib/aws-s3';
 import {BackupPlan, BackupPlanRule, BackupResource} from 'aws-cdk-lib/aws-backup';
-import {} from 'aws-cdk-lib/aws-iam';
+import {Role, ServicePrincipal, Policy, PolicyStatement, Effect} from 'aws-cdk-lib/aws-iam';
 import {Schedule} from 'aws-cdk-lib/aws-events';
 
 type BlogS3BackupInfrastructureProperties = {
@@ -25,15 +25,13 @@ export class BlogS3BackupInfrastructure extends Stack {
             autoDeleteObjects: true,
         });
 
-		Tags.of(bucket).add('backup', 'true');
-
 		const snapShotSchedule = new BackupPlanRule({
 			deleteAfter: Duration.days(7),
 			ruleName: 'daily-example-backup-7days-ret',
 			scheduleExpression: Schedule.cron({
 				month: '*',
 				day: '*',
-				hour: '5',
+				hour: '21',
 				minute: '0'
 			})
 		});
@@ -82,6 +80,133 @@ export class BlogS3BackupInfrastructure extends Stack {
 			}
 		);
 
+		const backupPlanRole = new Role(this, 's3-example-bucket-backup-role', {
+            assumedBy: new ServicePrincipal('backup.amazonaws.com'),
+        });
+
+        const awsS3BackupsCustomPolicy = new Policy(this, 's3-custom-aws-backup-policy', {
+            statements: [
+                new PolicyStatement({
+                    sid: 'S3BucketBackupPermissions',
+                    actions: [
+                        's3:GetInventoryConfiguration',
+                        's3:PutInventoryConfiguration',
+                        's3:ListBucketVersions',
+                        's3:ListBucket',
+                        's3:GetBucketVersioning',
+                        's3:GetBucketNotification',
+                        's3:PutBucketNotification',
+                        's3:GetBucketLocation',
+                        's3:GetBucketTagging',
+                    ],
+                    effect: Effect.ALLOW,
+                    resources: [bucket.bucketArn],
+                }),
+                new PolicyStatement({
+                    sid: 'S3ObjectBackupPermissions',
+                    actions: [
+                        's3:GetObjectAcl',
+                        's3:GetObject',
+                        's3:GetObjectVersionTagging',
+                        's3:GetObjectVersionAcl',
+                        's3:GetObjectTagging',
+                        's3:GetObjectVersion',
+                    ],
+                    effect: Effect.ALLOW,
+                    resources: [bucket.bucketArn, `${bucket.bucketArn}/*`],
+                }),
+                new PolicyStatement({
+                    sid: 'S3GlobalPermissions',
+                    actions: ['s3:ListAllMyBuckets'],
+                    effect: Effect.ALLOW,
+                    resources: ['*'],
+                }),
+                new PolicyStatement({
+                    sid: 'KMSBackupPermissions',
+                    actions: ['kms:Decrypt', 'kms:DescribeKey'],
+                    effect: Effect.ALLOW,
+                    resources: ['*'],
+                    conditions: {
+                        StringLike: {
+                            'kms:ViaService': 's3.*.amazonaws.com',
+                        },
+                    },
+                }),
+                new PolicyStatement({
+                    sid: 'EventsPermissions',
+                    actions: [
+                        'events:DescribeRule',
+                        'events:EnableRule',
+                        'events:PutRule',
+                        'events:DeleteRule',
+                        'events:PutTargets',
+                        'events:RemoveTargets',
+                        'events:ListTargetsByRule',
+                        'events:DisableRule',
+                    ],
+                    effect: Effect.ALLOW,
+                    resources: ['arn:aws:events:*:*:rule/AwsBackupManagedRule*'],
+                }),
+                new PolicyStatement({
+                    sid: 'EventsMetricsGlobalPermissions',
+                    actions: ['cloudwatch:GetMetricData', 'events:ListRules'],
+                    effect: Effect.ALLOW,
+                    resources: ['*'],
+                }),
+            ],
+        });
+
+        awsS3BackupsCustomPolicy.attachToRole(backupPlanRole);
+
+        const awsS3BackupsCustomRestorePolicy = new Policy(this, 's3-custom-aws-backup-restore-policy', {
+            statements: [
+                new PolicyStatement({
+                    sid: 'S3BucketRestorePermissions',
+                    actions: [
+                        's3:CreateBucket',
+                        's3:ListBucketVersions',
+                        's3:ListBucket',
+                        's3:GetBucketVersioning',
+                        's3:GetBucketLocation',
+                        's3:PutBucketVersioning',
+                    ],
+                    effect: Effect.ALLOW,
+                    resources: [bucket.bucketArn],
+                }),
+                new PolicyStatement({
+                    sid: 'S3ObjectRestorePermissions',
+                    actions: [
+                        's3:GetObject',
+                        's3:GetObjectVersion',
+                        's3:DeleteObject',
+                        's3:PutObjectVersionAcl',
+                        's3:GetObjectVersionAcl',
+                        's3:GetObjectTagging',
+                        's3:PutObjectTagging',
+                        's3:GetObjectAcl',
+                        's3:PutObjectAcl',
+                        's3:PutObject',
+                        's3:ListMultipartUploadParts',
+                    ],
+                    effect: Effect.ALLOW,
+                    resources: [bucket.bucketArn, `${bucket.bucketArn}/*`],
+                }),
+                new PolicyStatement({
+                    sid: 'KMSBackupPermissions',
+                    actions: ['kms:Decrypt', 'kms:DescribeKey', 'kms:GenerateDataKey'],
+                    effect: Effect.ALLOW,
+                    resources: ['*'],
+                    conditions: {
+                        StringLike: {
+                            'kms:ViaService': 's3.*.amazonaws.com',
+                        },
+                    },
+                }),
+            ],
+        });
+
+        awsS3BackupsCustomRestorePolicy.attachToRole(backupPlanRole);
+
 		backupPlan.addSelection(
 			's3-example-bucket',
 			{
@@ -89,7 +214,8 @@ export class BlogS3BackupInfrastructure extends Stack {
 					BackupResource.fromArn(bucket.bucketArn)
 				],
 				allowRestores: true,
-				backupSelectionName: 's3-example-bucket-only',
+				backupSelectionName: 's3-example-image-bucket-backup',
+				role: backupPlanRole
 			}
 		);
     }
